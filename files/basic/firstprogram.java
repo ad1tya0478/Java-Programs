eecf030e914d38c360757167155d45901f1d74e6
ad1tya0478 <aditya.50478@gmail.com>
2025-07-21 01:16:42 +0530

    Initial commit

---
+++ files/basic/firstprogram.java
@@ -0,0 +1,12 @@
+package basic;
+public class firstprogram {
+    public static void main(String a[]) {
+
+        float marks = 9.5f;
+        System.out.println(marks);
+
+        System.out.println("HELLO WORLD");
+
+    }
+
+}
