c27138a2c4bf98f2ad88bae9ca99aacdac09968c
ad1tya0478 <aditya.50478@gmail.com>
2025-08-04 22:32:56 +0530

    Did Pattern printing in java

--- Upcasting_Downcasting/interfaces.java
+++ Upcasting_Downcasting/interfaces.java
@@ -189,25 +189,4 @@ interface B{
         // System.out.println(result);
 
 
-//         for (int i=1;i<=5;i++){
-//                 for( int j=5;j>=i;j--){
-//                         System.out.print(j + " ");
-//                 }
-//                 System.out.println();
-//         }
-//         System.out.println();
-//         for (int i=1;i<=5;i++){
-//                 for( int j=1;j<=i;j++){
-//                         System.out.print(i + " ");
-//                 }
-//                 System.out.println();
-//         }
-//         System.out.println();
-//         for (int i=1;i<=5;i++){
-//                 for( int j=1;j<=i;j++){
-//                         System.out.print(j + " ");
-//                 }
-//                 System.out.println();
-//         }
-
 //     }
--- clg/index.java
+++ clg/index.java
@@ -1,14 +1,79 @@
 public class index {
 public static void main(String[] args) {
 
-    int num = 1;
-    while(num%2==0){
-        System.out.println(num);
-        num++;
-    }
+    // int num = 1;
+    // while(num%2==0){
+    //     System.out.println(num);
+    //     num++;
+    // }
 
-    
-    
+    // for (int i=1;i<=5;i++){
+    //             for( int j=5;j>=i;j--){
+    //                     System.out.print(j + " ");
+    //             }
+    //             System.out.println();
+    //     }
+    //     System.out.println();
+
+    //     for (int i=1;i<=5;i++){
+    //             for( int j=1;j<=i;j++){
+    //                     System.out.print(i + " ");
+    //             }
+    //             System.out.println();
+    //     }
+        // System.out.println();
+
+        // for (int i=1;i<=5;i++){
+        //         for( int j=1;j<=i;j++){
+        //                 System.out.print(j + " ");
+        //         }
+        //         System.out.println();
+        // }
+        // System.out.println();
+
+        // int number = 1;
+        // for (int i=1;i<=5;i++){
+        //         for( int j=1;j<=i;j++){
+        //                 System.out.print(number + " ");
+        //                 number++;
+        //         }
+        //         System.out.println();
+        // }
+        // System.out.println();
+
+
+        for (int i=5;i>=1;i--){
+                for( int j=1;j<=i;j++){
+                        System.out.print(j + " ");
+                }
+                System.out.println();
+        }
+        System.out.println();
+
+        for (int i=1;i<=5;i++){
+                for( int j=i;j<=5;j++){
+                    System.out.print(" ");
+                }
+                    for(int k = 1;k<=i;k++){
+                        System.out.print(k + " ");
+                    }
+                System.out.println();
+        }
+        System.out.println();
+
+        for (int i=1;i<=5;i++){
+                for( int j=i;j<=5;j++){
+                    System.out.print("  ");
+                }
+                    for(int k = 1;k<=i;k++){
+                        System.out.print(k + " ");
+                    }
+                System.out.println();
+        }
+        System.out.println();
+
+        
+
 
     }
 }
