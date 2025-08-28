5b28723a64cd063c5c3e97b9d6ed673f12bde9ef
ad1tya0478 <aditya.50478@gmail.com>
2025-08-28 23:00:54 +0530

    Search in Rotated sorted array

--- kunal-kushwah/Searching/questions/ques_7.java
+++ kunal-kushwah/Searching/questions/ques_7.java
@@ -1,58 +1,80 @@
+// Search in rotated sorted array
 public class ques_7 {
     public static void main(String[] args) {
 
-        int[] arr = {3,4,5,6,7,0,1,2};
+        int[] arr = {4,5,6,7,0,1,2};
         int target = 2;
-        System.out.println(pivot(arr));
+        System.out.println(findPivot(arr));
 
     }
 
-    static int pivot(int[] arr){
+    
+    // static int pivot(int[] arr){
+    //     int pivot = arr[0];
+    //     for(int i = 1; i < arr.length; i++){
+    //         if(pivot<arr[i]){
+    //             pivot = arr[i];
+    //         }
+    //     }
+    //     return pivot;
+    // }
 
-        int pivot = arr[0];
-        for(int i = 1; i < arr.length; i++){
-            if(pivot<arr[i]){
-                pivot = arr[i];
+    static int search(int[] arr, int target){
+        int pivot = findPivot(arr);
+
+        // if you did not find a pivot, it means array is not rotated
+        if(pivot == -1){
+            // just do normal binary search
+            return BinarySearch(arr, target, 0, arr.length-1);
+        }
+        // if pivot is found, you have found 2 asc sorted arrays 
+        if(arr[pivot] == target){
+            return pivot;
+        }
+        if(target > arr[0]) {
+            return BinarySearch(arr, target, 0, pivot-1);
+        }
+        return BinarySearch(arr, target, pivot+1, arr.length-1);
+    }
+
+    static int BinarySearch(int[] arr, int target, int start, int end){
+
+        while(start <= end){
+        int mid = start + (end - start) / 2; 
+
+            if(target < arr[mid]){
+                end = mid - 1;
+            } else if(target > arr[mid]){
+                start = mid + 1;
+            } else {
+                // ans found
+                return mid;
+            }
+
+        }
+        return -1;
+
+    }
+
+    static int findPivot(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start + (end - start) / 2; 
+            // 4 cases over here
+            if(mid < end && arr[mid] > arr[mid+1]){
+                return mid;
+            }
+            if(mid > start && arr[mid] < arr[mid-1]){
+                return mid-1; 
+            }
+            if(arr[mid] <= arr[start]){
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
         }
-
-
-
-        return pivot;
-    }
-
-    static int BinarySearch(int[] arr, int target, int left, int right){
-
-     left = 0;
-     right = arr.length - 1;
-
-     // find whether the array is sorted in ascending or descending
-     boolean isAsc = arr[left] < arr[right];
-
-     while(left <= right){
-
-         int mid = left + (right - left) / 2; 
-
-         if(arr[mid] == target){
-             return mid;
-         }
-
-         if(isAsc){
-             if (target < arr[mid]){
-                 right = mid - 1;
-             } else {
-                 left = mid + 1;
-             } 
-         } else {
-             if(target > arr[mid]){
-                 right = mid - 1;
-             } else {
-                 left = mid + 1;
-             } 
-         }
-     }
-
-     return -1;
- } 
+        return -1;
+    } 
 
 }
